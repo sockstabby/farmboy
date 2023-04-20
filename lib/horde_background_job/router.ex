d8f2c061023ca318e065c803a52e5334dfda359c
Your Name <you@example.com>
2023-04-20 16:11:36 -0500

    lots of cleanup

--- lib/horde_background_job/router.ex
+++ lib/horde_background_job/router.ex
@@ -16,103 +16,31 @@ defmodule HordeTaskRouter.Router do
     tname = via_tuple(name)
     IO.inspect( tname )
     timeout = Keyword.get(opts, :timeout, @default_timeout)
-
-    #GenServer.start_link(__MODULE__, timeout, name: name)
-
     GenServer.start_link(__MODULE__, timeout, name: via_tuple(name))
-
   end
 
 
   def monitor_global_task(nil) do
     Logger.debug("monitor_global_task nil")
-
-    schedule(10_000)
-
   end
 
   def monitor_global_task( [ task | _tail] ) do
     Logger.debug("monitor_global_task []")
-
     %{pid: pid } = task
-
      Logger.debug("pid = ")
-
      IO.inspect(pid)
-
      Process.link(pid)
-
   end
 
-  @impl GenServer
+  @impl true
   @spec init(non_neg_integer) :: {:ok, non_neg_integer}
   def init(timeout) do
-    Logger.debug("initializing")
-
     Process.flag(:trap_exit, true)
-
-    Logger.debug("before")
-
     value = get_global_tasks()
-
     monitor_global_task(value)
-
-
-    #Logger.debug("value = ")
-    #IO.inspect(value)
-    #Logger.debug("after")
     Logger.debug("global tasks = ")
     IO.inspect(value)
-
-
-
-    #IO.inspect(tasks)
-    #schedule(timeout)
     {:ok, timeout}
-  end
-
-
-
-  @impl GenServer
-  def handle_info(:execute, timeout) do
-
-    #value = get_global_tasks()
-
-    Logger.debug("handle_info :execute")
-
-    # foo is the the first_dist task project
-    #task = Task.Supervisor.async({Chat.TaskSupervisor, :foo@localhost}, FirstDistributedTask, :hello, [12])
-
-    task = Task.Supervisor.async_nolink({Chat.TaskSupervisor, :foo@localhost}, FirstDistributedTask, :hello, [12])
-
-    IO.inspect(task)
-
-
-    Horde.Registry.put_meta(HordeTaskRouter.HordeRegistry, "tasks", [task])
-
-    #Process.sleep(1000 * 10)
-
-    #GenServer.cast({:via, Horde.Registry, {HordeTaskRouter.HordeRegistry, "client"}}, {:monitor_task, task})
-
-    #IO.inspect(task)
-
-    #client12@127.0.0.1
-
-    #Horde.Registry.put_meta(HordeTaskRouter.TaskRegistry, "tasks", [task])
-
-
-
-    #Logger.debug("after put reading")
-
-    #Process.sleep(5000)
-
-    #{:ok, tasks } = Horde.Registry.meta(HordeTaskRouter.TaskRegistry, "tasks")
-
-    #IO.inspect(tasks)
-
-    #Logger.debug("after reading")
-
-    {:noreply, timeout}
   end
 
   @impl true
@@ -130,26 +58,21 @@ defmodule HordeTaskRouter.Router do
   end
 
   @impl GenServer
-  def handle_cast({:run_task, %{object: o, method: m, args: a} }, state) do
+  def handle_cast({:run_task, %{object: _o, method: _m, args: _a} }, state) do
     Logger.debug("handle_cast")
+    #Logger.debug("o=#{o}")
+    #Logger.debug("m=#{m}")
+    #IO.puts("a =")
+    #IO.inspect(a)
 
-    Logger.debug("o=#{o}")
-    Logger.debug("m=#{m}")
-
-    IO.puts("a =")
-    IO.inspect(a)
+    task = Task.Supervisor.async_nolink({Chat.TaskSupervisor, :foo@localhost}, FirstDistributedTask, :hello, [12])
+    IO.inspect(task)
+    Horde.Registry.put_meta(HordeTaskRouter.HordeRegistry, "tasks", [task])
 
     {:noreply, state}
   end
 
-  defp schedule(timeout) do
-    log("scheduling for #{timeout}ms")
-    Process.send_after(self(), :execute, timeout)
-  end
 
-  defp log(text) do
-    Logger.info("----[#{node()}-#{inspect(self())}] #{__MODULE__} #{text}")
-  end
 
   def via_tuple(name), do: {:via, Horde.Registry, {HordeTaskRouter.HordeRegistry, name}}
 
