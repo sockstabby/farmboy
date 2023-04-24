8aa4ee38099400de8dc1e9b4c9a03fe02fba08ab
Your Name <you@example.com>
2023-04-23 20:25:55 -0500

    good stuff

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HordeTaskRouter.MixProject do
 
   def project do
     [
-      app: :horde_background_job,
+      app: :task_router,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
